8cc0c442b3edaa3def34e324b90041e05ddf5ded
Abir Banerjee <abirbanerjee793@gmail.com>
2025-01-18 10:29:26 +0530

    entry method added

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 import updateBook
 
-while True:
-    subject_code = int(input('Which subject class?\nsubject1\nsubject2\nsubject3\n\n'))
-    absentes = list(map(int, input("Enter roll numbers").split(' ')))
-    
-    if len(absentes) > 0:
-        updateBook.handle(subject_code=subject_code, roll_absentees=absentes)
-    else:
-        print('No absentess entered for this subject class')
-    
-    exit = int(input("Enter 1 to continue or 0 to exit"))
-    if exit == 0:
-        print('Exiting...')
-        break 
+def main():
+    while True:
+        
+        subject_code = int(input("Which subject class?\n1. subject1\n2. subject2\n3. subject3\n\n"))
+        
+       
+        absentees = list(map(int, input("Enter roll numbers separated by spaces: ").split()))
+        
+        
+        if len(absentees) > 0:
+            updateBook.handle(subject_code=subject_code, roll_absentees=absentees)
+        else:
+            print("No absentees entered for this subject class.")
+        
+       
+        choice = int(input("Enter 1 to continue or 0 to exit: "))
+        if choice == 0:
+            print("Exiting...")
+            break
+
+if __name__ == '__main__':
+    main()
